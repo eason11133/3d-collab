8a3667fffe7fdb17ee3be00592dcf7a0df7e8cb5
Eason Huang <eason456h@gmail.com>
2025-09-14 14:01:05 +0800

    chore(vite): optimizeDeps include three exporters & three-csg-ts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2020'
+  },
+  optimizeDeps: {
+    include: [
+      'three',
+      'three/examples/jsm/exporters/GLTFExporter.js',
+      'three/examples/jsm/exporters/STLExporter.js',
+      'three-csg-ts'
+    ]
+  }
 })
